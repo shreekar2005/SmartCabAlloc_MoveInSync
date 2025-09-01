f723e76b19dd68af8cfc37db74a0fcf20080639c
shreekar <shreekarmane7@gmail.com>
2025-09-01 20:44:22 +0530

    updating code part in employee routes

--- app/employee/routes.py
+++ app/employee/routes.py
@@ -180,67 +180,67 @@ def re_request_trip(trip_id):
     }), 200
 
 
-@employee_bp.route('/cabs/nearby', methods=['GET','POST'])
-@jwt_required()
-def get_nearby_engaged_cabs():
-    lat = request.args.get('lat', type=float)
-    lon = request.args.get('lon', type=float)
-    radius_km = request.args.get('radius', type=float, default=5.0)
+# @employee_bp.route('/cabs/nearby', methods=['GET','POST'])
+# @jwt_required()
+# def get_nearby_engaged_cabs():
+#     lat = request.args.get('lat', type=float)
+#     lon = request.args.get('lon', type=float)
+#     radius_km = request.args.get('radius', type=float, default=5.0)
 
-    if lat is None or lon is None:
-        return jsonify({"message": "Latitude and longitude are required parameters"}), 400
+#     if lat is None or lon is None:
+#         return jsonify({"message": "Latitude and longitude are required parameters"}), 400
 
-    # This logic specifically implements the requirement to show ONLY engaged cabs
-    engaged_cabs = Cab.query.filter_by(status='on_trip').all()
+#     # This logic specifically implements the requirement to show ONLY engaged cabs
+#     engaged_cabs = Cab.query.filter_by(status='on_trip').all()
     
-    graph = load_road_network()
-    if not graph:
-        return jsonify({"message": "Road network not available"}), 503
+#     graph = load_road_network()
+#     if not graph:
+#         return jsonify({"message": "Road network not available"}), 503
 
-    employee_coords = (lat, lon)
-    nearby_cabs = []
+#     employee_coords = (lat, lon)
+#     nearby_cabs = []
 
-    for cab in engaged_cabs:
-        cab_coords = (cab.current_lat, cab.current_lon)
-        distance_meters = find_shortest_path_distance(graph, employee_coords, cab_coords)
+#     for cab in engaged_cabs:
+#         cab_coords = (cab.current_lat, cab.current_lon)
+#         distance_meters = find_shortest_path_distance(graph, employee_coords, cab_coords)
         
-        if distance_meters <= radius_km * 1000:
-            nearby_cabs.append({
-                "id": cab.id,
-                "driver_name": cab.driver_name,
-                "license_plate": cab.license_plate,
-                "lat": cab.current_lat,
-                "lon": cab.current_lon,
-                "status": cab.status,
-                "distance_meters": round(distance_meters, 2)
-            })
+#         if distance_meters <= radius_km * 1000:
+#             nearby_cabs.append({
+#                 "id": cab.id,
+#                 "driver_name": cab.driver_name,
+#                 "license_plate": cab.license_plate,
+#                 "lat": cab.current_lat,
+#                 "lon": cab.current_lon,
+#                 "status": cab.status,
+#                 "distance_meters": round(distance_meters, 2)
+#             })
 
-    # Sort by distance
-    nearby_cabs.sort(key=lambda x: x['distance_meters'])
+#     # Sort by distance
+#     nearby_cabs.sort(key=lambda x: x['distance_meters'])
 
-    return jsonify(nearby_cabs), 200
+#     return jsonify(nearby_cabs), 200
 
-@employee_bp.route('/update-location', methods=['POST'])
-@jwt_required()
-def update_location():
-    data = request.get_json()
-    lat = data.get('lat')
-    lon = data.get('lon')
+# @employee_bp.route('/update-location', methods=['POST'])
+# @jwt_required()
+# def update_location():
+#     data = request.get_json()
+#     lat = data.get('lat')
+#     lon = data.get('lon')
 
-    if not lat or not lon:
-        return jsonify({"message": "Latitude and longitude are required"}), 400
+#     if not lat or not lon:
+#         return jsonify({"message": "Latitude and longitude are required"}), 400
 
-    current_user_public_id = get_jwt_identity()
-    user = User.query.filter_by(public_id=current_user_public_id).first()
+#     current_user_public_id = get_jwt_identity()
+#     user = User.query.filter_by(public_id=current_user_public_id).first()
 
-    if not user:
-        return jsonify({"message": "User not found"}), 404
+#     if not user:
+#         return jsonify({"message": "User not found"}), 404
 
-    user.latitude = lat
-    user.longitude = lon
-    db.session.commit()
+#     user.latitude = lat
+#     user.longitude = lon
+#     db.session.commit()
 
-    return jsonify({"message": "Location updated successfully"}), 200
+#     return jsonify({"message": "Location updated successfully"}), 200
 
 @employee_bp.route('/trips/finish', methods=['POST'])
 @jwt_required()
